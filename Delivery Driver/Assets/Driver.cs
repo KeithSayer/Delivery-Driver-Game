812c788a21229d14e5fb1a7f3d7cf3abdf4f357c
Keith Sayer <keithasayer@gmail.com>
2022-11-27 21:04:55 -0500

    update axis controls

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = .1f;
+    [SerializeField] float steerSpeed = .5f;
     [SerializeField] float moveSpeed = .01f;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, moveSpeed, 0);
-        transform.Rotate(0, 0, steerSpeed);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, -steerAmount);
     }
 }
